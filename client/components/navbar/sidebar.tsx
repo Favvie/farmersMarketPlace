4b2a00817b2c6c8a51cb4c7eb645ec3afa1fc30f
Obaloluwa Olusoji <olusojiobah@gmail.com>
2024-10-13 13:57:40 +0100

    feat: implemented sidebar

---
+++ client/components/navbar/sidebar.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { SIDEBAR_ITEMS } from "@/lib/constants";
+import { NavItemWrapper } from "./nav-item-wrapper";
+import { usePathname } from "next/navigation";
+import { Icons } from "../icons";
+import { Button } from "../ui/button";
+
+export function Sidebar({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const userAddress = "0x6f..813A65";
+
+  return (
+    <div className="grid min-h-screen w-full lg:grid-cols-[310px_1fr]">
+      <aside className="sticky top-0 h-screen bg-blue-2">
+        <div className="flex h-full max-h-screen flex-col p-6">
+          <Icons.logo />
+
+          <nav className="flex flex-1 flex-col justify-between overflow-auto">
+            <ul className="space-y-[12px] py-2 px-4">
+              <li>
+                <NavItemWrapper item={SIDEBAR_ITEMS[0]} pathname={pathname}>
+                  <Icons.home
+                    stroke={
+                      pathname === SIDEBAR_ITEMS[0].href ? "#448DF2" : "#5D6679"
+                    }
+                  />
+                </NavItemWrapper>
+              </li>
+
+              <li>
+                <NavItemWrapper item={SIDEBAR_ITEMS[1]} pathname={pathname}>
+                  <Icons.cart
+                    fill={
+                      pathname === SIDEBAR_ITEMS[1].href ? "#448DF2" : "#5D6679"
+                    }
+                  />
+                </NavItemWrapper>
+              </li>
+
+              <li>
+                <NavItemWrapper item={SIDEBAR_ITEMS[2]} pathname={pathname}>
+                  <Icons.report
+                    fill={
+                      pathname === SIDEBAR_ITEMS[2].href ? "#448DF2" : "#5D6679"
+                    }
+                  />
+                </NavItemWrapper>
+              </li>
+
+              <li>
+                <NavItemWrapper item={SIDEBAR_ITEMS[3]} pathname={pathname}>
+                  <Icons.box
+                    fill={
+                      pathname === SIDEBAR_ITEMS[3].href ? "#448DF2" : "#5D6679"
+                    }
+                  />
+                </NavItemWrapper>
+              </li>
+            </ul>
+
+            <Button
+              variant={"ghost"}
+              className="w-[186px] gap-4 h-auto py-0 justify-start text-gray-0 hover:text-blue-0 hover:bg-white text-base"
+            >
+              <Icons.logout />
+              <span>{userAddress}</span>
+            </Button>
+          </nav>
+        </div>
+      </aside>
+      <>{children}</>
+    </div>
+  );
+}
---
+++ client/lib/constants.ts
@@ -0,0 +1,20 @@
+import { INavbar } from "./types";
+
+export const SIDEBAR_ITEMS: INavbar[] = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    title: "My Listing",
+    href: "/dashboard/listing",
+  },
+  {
+    title: "Reports",
+    href: "/dashboard/reports",
+  },
+  {
+    title: "Orders",
+    href: "/dashboard/orders",
+  },
+];
