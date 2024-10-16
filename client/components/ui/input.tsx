08c876fe0830fbc0ca1b3c98e0362e391f0e0050
Gifftybabe <ulokangozi@gmail.com>
2024-10-16 23:11:33 +0100

    fix: resolve build error

--- client/components/ui/input.tsx
+++ client/components/ui/input.tsx
@@ -1,10 +1,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-
-
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
