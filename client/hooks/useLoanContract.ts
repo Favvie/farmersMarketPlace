f610e10e117fb0da09300c7a7fb9f6108b16e6e8
Edoscoba <obisimon88@gmail.com>
2024-10-18 13:07:06 +0100

    deployment script

--- client/hooks/useLoanContract.ts
+++ client/hooks/useLoanContract.ts
@@ -1,15 +1,16 @@
 import { client } from "@/utils/client";
 import { useCallback } from "react";
-import { getContract } from "thirdweb";
-import { baseSepolia } from "thirdweb/chains";
+import { defineChain, getContract } from "thirdweb";
+
+const liskSepolia = defineChain(4202);
 
 export const useLoanContract = () => {
   return useCallback(
     () =>
       getContract({
         client,
-        address: "",
-        chain: baseSepolia,
+        address: "0xe76465828cB5cdB27a57C2b8E75fdeEefFC1f0e3",
+        chain: liskSepolia,
       }),
     []
   );
