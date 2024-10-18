c2981a2d1cc6fce61cc4460c7356c03512d91a14
Favvie <favourabangwu@gmail.com>
2024-10-18 12:08:07 +0100

    feat: upload seller page

--- client/components/navbar/sidebar.tsx
+++ client/components/navbar/sidebar.tsx
@@ -8,72 +8,71 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 
 export function Sidebar({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  const userAddress = "0x6f..813A65";
+	const userAddress = "0x6f..813A65";
 
-  return (
-    <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
-      <aside className="sticky top-0 h-screen bg-blue-2">
-        <div className="flex h-full max-h-screen flex-col p-6">
-          <Link href={"/"}>
-            <Icons.logo />
-          </Link>
+	return (
+		<div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
+			<aside className="sticky top-0 h-screen bg-blue-2">
+				<div className="flex h-full max-h-screen flex-col p-6">
+					<Link href={"/"}>
+						<Icons.logo />
+					</Link>
 
-          <nav className="flex flex-1 flex-col justify-between overflow-auto">
-            <ul className="space-y-[12px] py-2 px-4">
-              <li>
-                <NavItemWrapper item={SIDEBAR_ITEMS[0]} pathname={pathname}>
-                  <Icons.home
-                    stroke={
-                      pathname === SIDEBAR_ITEMS[0].href ? "#4ABA4F" : "#5D6679"
-                    }
-                  />
-                </NavItemWrapper>
-              </li>
+					<nav className="flex flex-1 flex-col justify-between overflow-auto">
+						<ul className="space-y-[12px] py-2 px-4">
+							<li>
+								<NavItemWrapper
+									item={SIDEBAR_ITEMS[0]}
+									pathname={pathname}>
+									<Icons.home stroke={pathname === SIDEBAR_ITEMS[0].href ? "#4ABA4F" : "#5D6679"} />
+								</NavItemWrapper>
+							</li>
 
-              <li>
-                <NavItemWrapper item={SIDEBAR_ITEMS[1]} pathname={pathname}>
-                  <Icons.trolley
-                    fill={
-                      pathname === SIDEBAR_ITEMS[1].href ? "#448DF2" : "#5D6679"
-                    }
-                  />
-                </NavItemWrapper>
-              </li>
+							<li>
+								<NavItemWrapper
+									item={SIDEBAR_ITEMS[1]}
+									pathname={pathname}>
+									<Icons.trolley fill={pathname === SIDEBAR_ITEMS[1].href ? "#448DF2" : "#5D6679"} />
+								</NavItemWrapper>
+							</li>
 
-              <li>
-                <NavItemWrapper item={SIDEBAR_ITEMS[2]} pathname={pathname}>
-                  <Icons.report
-                    fill={
-                      pathname === SIDEBAR_ITEMS[2].href ? "#448DF2" : "#5D6679"
-                    }
-                  />
-                </NavItemWrapper>
-              </li>
+							<li>
+								<NavItemWrapper
+									item={SIDEBAR_ITEMS[2]}
+									pathname={pathname}>
+									<Icons.report fill={pathname === SIDEBAR_ITEMS[2].href ? "#448DF2" : "#5D6679"} />
+								</NavItemWrapper>
+							</li>
 
-              <li>
-                <NavItemWrapper item={SIDEBAR_ITEMS[3]} pathname={pathname}>
-                  <Icons.box
-                    fill={
-                      pathname === SIDEBAR_ITEMS[3].href ? "#448DF2" : "#5D6679"
-                    }
-                  />
-                </NavItemWrapper>
-              </li>
-            </ul>
+							<li>
+								<NavItemWrapper
+									item={SIDEBAR_ITEMS[3]}
+									pathname={pathname}>
+									<Icons.box fill={pathname === SIDEBAR_ITEMS[3].href ? "#448DF2" : "#5D6679"} />
+								</NavItemWrapper>
+							</li>
 
-            <Button
-              variant={"ghost"}
-              className="w-[186px] gap-4 h-auto py-0 justify-start text-gray-0 hover:text-blue-0 hover:bg-white text-base"
-            >
-              <Icons.logout />
-              <span>{userAddress}</span>
-            </Button>
-          </nav>
-        </div>
-      </aside>
-      <main className="bg-[#F0F1F3] pb-20">{children}</main>
-    </div>
-  );
+							<li>
+								<NavItemWrapper
+									item={SIDEBAR_ITEMS[4]}
+									pathname={pathname}>
+									<Icons.box fill={pathname === SIDEBAR_ITEMS[4].href ? "#448DF2" : "#5D6679"} />
+								</NavItemWrapper>
+							</li>
+						</ul>
+
+						<Button
+							variant={"ghost"}
+							className="w-[186px] gap-4 h-auto py-0 justify-start text-gray-0 hover:text-blue-0 hover:bg-white text-base">
+							<Icons.logout />
+							<span>{userAddress}</span>
+						</Button>
+					</nav>
+				</div>
+			</aside>
+			<main className="bg-[#F0F1F3] pb-20">{children}</main>
+		</div>
+	);
 }
--- client/lib/constants.ts
+++ client/lib/constants.ts
@@ -1,27 +1,26 @@
 import { INavbar } from "./types";
 
 export const SIDEBAR_ITEMS: INavbar[] = [
-  {
-    title: "Dashboard",
-    href: "/dashboard",
-  },
-  {
-    title: "My Listing",
-    href: "/dashboard/listing",
-  },
-  {
-    title: "Reports",
-    href: "/dashboard/reports",
-  },
-  {
-    title: "Orders",
-    href: "/dashboard/orders",
-  },
+	{
+		title: "Dashboard",
+		href: "/dashboard",
+	},
+	{
+		title: "My Listing",
+		href: "/dashboard/listing",
+	},
+	{
+		title: "Reports",
+		href: "/dashboard/reports",
+	},
+	{
+		title: "Orders",
+		href: "/dashboard/orders",
+	},
+	{
+		title: "Loans",
+		href: "/dashboard/microloans",
+	},
 ];
 
-export const PRODUCT_SALES_TABLE_HEADER = [
-  "Product",
-  "Product ID",
-  "Category",
-  "Remaining Quantity",
-];
+export const PRODUCT_SALES_TABLE_HEADER = ["Product", "Product ID", "Category", "Remaining Quantity"];
